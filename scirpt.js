b5b63ae5cc71c3e95a3a9c933ca5ae814c94b264
Jam Hejrati <jamhejratipro@gmail.com>
2022-02-15 23:24:40 +0330

    menu edit

--- scirpt.js
+++ scirpt.js
@@ -1,11 +1,11 @@
-const menuNavbar = document.querySelector(".menu");
-const toggleBtn = document.querySelector(".toggle-arrow");
-const arrowBtn = document.querySelector(".fa-arrow-left");
-const menuIcon = document.querySelector(".menu-icon");
+// const menuNavbar = document.querySelector(".menu");
+// const toggleBtn = document.querySelector(".toggle-arrow");
+// const arrowBtn = document.querySelector(".fa-arrow-left");
+// const menuIcon = document.querySelector(".menu-icon");
 
-const navToggleHandler = ()=>{
-    arrowBtn.style.transform = 'rotate(180deg)';
-    // menuIcon.textContent = '';
-}
+// const navToggleHandler = ()=>{
+//     arrowBtn.style.transform = 'rotate(180deg)';
+//     menuIcon.innerHTML = '';
+// }
 
-toggleBtn.addEventListener('click',navToggleHandler);
+// toggleBtn.addEventListener('click',navToggleHandler);
